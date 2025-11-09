a35d271bed34ffb3a05fce7413703cb2d61e72d1
SoHiEarth <126925211+SoHiEarth@users.noreply.github.com>
2025-11-09 18:27:21 +0900

    feat: Initial commit

---
+++ src/main.cc
@@ -0,0 +1,36 @@
+// Code block
+#include <glad/glad.h>
+// Code block
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+  auto window = glfwCreateWindow(800, 600, "ion", NULL, NULL);
+  if (window == nullptr) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glViewport(0, 0, 800, 600);
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
